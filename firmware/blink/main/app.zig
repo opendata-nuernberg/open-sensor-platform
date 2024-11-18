27de592074a25786a26e0ecee01548f91523c415
Jens <jens.saalmueller@codecentric.de>
2024-11-19 00:12:49 +0100

    feat: add i2c code for connecting to sht4x (wip)

--- firmware/blink/main/app.zig
+++ firmware/blink/main/app.zig
@@ -3,6 +3,8 @@ const builtin = @import("builtin");
 const idf = @import("esp_idf");
 const OnboardLed = @import("onboard_led.zig").OnboardLed;
 
+const i2c = idf.i2c;
+
 export fn app_main() callconv(.C) void {
     // This allocator is safe to use as the backing allocator w/ arena allocator
     // std.heap.raw_c_allocator
@@ -76,6 +78,10 @@ export fn app_main() callconv(.C) void {
     if (idf.xTaskCreate(blinkclock, "blink", 1024 * 2, null, 5, null) == 0) {
         @panic("Error: Task blinkclock not created!\n");
     }
+
+    if (idf.xTaskCreate(sht4x_task, "sht4x", 1024 * 6, null, 4, null) == 0) {
+        @panic("Error: Task sht4x_task not created!\n");
+    }
 }
 
 // comptime function
@@ -114,7 +120,44 @@ fn arraylist(allocator: std.mem.Allocator) !void {
         );
     }
 }
+
 // Task functions (must be exported to C ABI) - runtime functions
+
+export fn sht4x_task(_: ?*anyopaque) void {
+    const i2cmb: idf.sys.i2c_master_bus_config_t = .{
+        .i2c_port = 0,
+        .flags = std.mem.zeroes(usize),
+        .sda_io_num = .GPIO_NUM_5,
+        .scl_io_num = .GPIO_NUM_6,
+        .clk_source = idf.sys.i2c_clock_source_t.I2C_CLK_SRC_APB,
+        .glitch_ignore_cnt = 7,
+    };
+    var i2c_bus_handle: idf.sys.i2c_master_bus_handle_t = undefined;
+    i2c.BUS.add(&i2cmb, &i2c_bus_handle) catch unreachable;
+
+    const i2c_device_sht4x_config: idf.sys.i2c_device_config_t = .{
+        .dev_addr_length = idf.sys.i2c_addr_bit_len_t.I2C_ADDR_BIT_LEN_7,
+        .device_address = 0x44,
+        .scl_speed_hz = 400_000,
+    };
+    var i2c_device_handle: idf.sys.i2c_master_dev_handle_t = undefined;
+    i2c.BUS.addDevice(i2c_bus_handle, &i2c_device_sht4x_config, &i2c_device_handle) catch unreachable;
+    defer i2c.removeDevice(&i2c_device_handle);
+
+    const DATA_LENGTH = 20;
+    var data: [DATA_LENGTH:0]u8 = [_:0]u8{0} ** DATA_LENGTH;
+    //const data: [DATA_LENGTH:0]u8 = std.mem.zeroes([DATA_LENGTH:0]u8);
+    const data_rd: [*:0]u8 = &data;
+
+    while (true) {
+        //_ = idf.sys.i2c_master_receive(i2c_device_handle, data_rd, DATA_LENGTH, -1);
+        log.info("Test Value: {x}", .{100});
+        data_rd[0] = data_rd[0] + 1;
+        log.info("Value: {x}", .{data[0]});
+        idf.vTaskDelay(2000 / idf.portTICK_PERIOD_MS);
+    }
+}
+
 export fn blinkclock(_: ?*anyopaque) void {
     blinkLED(1000) catch |err|
         @panic(@errorName(err));
