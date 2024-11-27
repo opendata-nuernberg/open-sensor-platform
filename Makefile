7e28f5e1511c70fc4e6e5a3e6d0ab416bfd04557
Jens <jens.saalmueller@codecentric.de>
2024-11-27 21:37:12 +0100

    fix: change flash size to 4MB

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ build_firmware:
 flash_firmware:
 	@echo "Flashing firmware"
 	esptool --chip esp32c3 -b 460800 --before default_reset --after hard_reset write_flash \
-	--flash_mode dio --flash_size 2MB --flash_freq 80m \
+	--flash_mode dio --flash_size 4MB --flash_freq 80m \
 	0x0 ${BOOTLOADER_PATH} 0x8000 ${PARTITION_TABLE_PATH} 0x10000 ${FIRMWARE_PATH}
 
 monitor:
--- tasks.py
+++ tasks.py
@@ -12,7 +12,7 @@ def flash_firmware(c):
     partition_table = os.path.join(BUILD_PATH, "partition_table", "partition-table.bin")
     firmware = os.path.join(BUILD_PATH, "zig-sample-idf.bin")
     c.run(
-        f"esptool.py --chip esp32c3 -b 460800 --before default_reset --after hard_reset write_flash --flash_mode dio --flash_size 2MB --flash_freq 80m 0x0 {bootloader} 0x8000 {partition_table} 0x10000 {firmware}"
+        f"esptool.py --chip esp32c3 -b 460800 --before default_reset --after hard_reset write_flash --flash_mode dio --flash_size 4MB --flash_freq 80m 0x0 {bootloader} 0x8000 {partition_table} 0x10000 {firmware}"
     )
 
 
