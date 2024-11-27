33170a0d4e0cf260ddca70ee34e668ce2780607d
Jan Beelte <jan.beelte@mailbox.org>
2024-11-27 20:10:23 +0100

    (add) new sample that should use the extracted library

---
+++ firmware/blink_idf/main/onboard_led.zig
@@ -0,0 +1,22 @@
+const idf = @import("esp_idf");
+
+pub const OnboardLed = struct {
+    const Self = @This();
+
+    pin: idf.sys.gpio_num_t = .GPIO_NUM_0,
+
+    pub fn init(self: Self) !void {
+        try idf.gpio.Direction.set(
+            self.pin,
+            .GPIO_MODE_OUTPUT,
+        );
+    }
+
+    pub fn on(self: Self) !void {
+        try idf.gpio.Level.set(self.pin, 1);
+    }
+
+    pub fn off(self: Self) !void {
+        try idf.gpio.Level.set(self.pin, 0);
+    }
+};
