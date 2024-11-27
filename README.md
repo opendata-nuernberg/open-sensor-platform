2b25f803ce3a35a3c3798568714a615aae1a859f
Jan Beelte <jan.beelte@mailbox.org>
2024-11-27 18:19:58 +0000

    (add) invoke script to flash and monitor serial.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # open-sensor-platform
 Hardware Designs and Firmware of the Open Sensor Platform
+
+
+## Installation
+### pyinvoke and tab-completion
+- MacOS: `brew install pyinvoke`
+- Linux: Use your system package manager
+- Other platforms: `pip install invoke`
+
+- [Generate tab-completion script](https://docs.pyinvoke.org/en/stable/invoke.html#generating-a-completion-script)
+
+### tio
+- MacOS: `brew install tio`
+- Linux: Use your system package manager
---
+++ tasks.py
@@ -0,0 +1,21 @@
+import os
+import pty
+
+from invoke import task
+
+BUILD_PATH = os.path.join("firmware", "blink", "build")
+
+
+@task
+def flash_firmware(c):
+    bootloader = os.path.join(BUILD_PATH, "bootloader", "bootloader.bin")
+    partition_table = os.path.join(BUILD_PATH, "partition_table", "partition-table.bin")
+    firmware = os.path.join(BUILD_PATH, "zig-sample-idf.bin")
+    c.run(
+        f"esptool.py --chip esp32c3 -b 460800 --before default_reset --after hard_reset write_flash --flash_mode dio --flash_size 2MB --flash_freq 80m 0x0 {bootloader} 0x8000 {partition_table} 0x10000 {firmware}"
+    )
+
+
+@task
+def monitor(c):
+    c.run("tio --auto-connect latest", pty=True)
